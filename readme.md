b0c684fd1b9c3f50dcbb2e44de4fbcdb2624daf1
favoursyre <favourforchrist91@gmail.com>
2022-08-16 20:43:55 +0100

    commit

---
+++ readme.md
@@ -0,0 +1,50 @@
+# VM Ware Detector
+
+## Disclaimer
+
+This script is for educational purposes only, I don't endorse or promote it's illegal usage
+
+## Table of Contents
+
+1. [Overview](#overview)
+2. [Features](#features)
+3. [Languages](#languages)
+4. [Installations](#installations)
+5. [Usage](#usage)
+6. [Run](#run)
+
+## Overview
+
+This script checks to see if the software is running in a virtual machine
+
+## Features
+
+- It checks to see if the software is running in VM
+
+## Languages
+
+- Python 3.9.7
+
+## Installations
+
+```shell
+git clone https://github.com/favoursyre/vm-detector.git && cd vm-detector
+```
+
+```shell
+pip install scapy
+```
+
+## Usage
+
+Instantiating the class
+
+```python
+detect = status()
+```
+
+## Run
+
+```shell
+python vm_detector.py
+```
---
+++ vm_detector.py
@@ -0,0 +1,74 @@
+#I want to create a Virtual Machine Detector
+
+#Useful libraries that I would be working with -->
+import os
+import sys 
+import subprocess 
+import re 
+import uuid
+import socket
+import ctypes
+import time
+import requests
+import urllib.request
+from scapy.all import *
+
+
+#This function helps get the vendor of a specified mac address
+def macVendor(mac_address):
+    #This loops helps to handle connection error and retries connecting 
+    count = 0
+    while True:
+        try:
+            url = "https://api.macvendors.com/"
+            response = urllib.request.urlopen(url + mac_address).read().decode('utf8')
+            print("got mac response successfully")
+            break
+        except:
+            url = "https://api.macvendors.com/"
+            print("response wasn't successfully, retrying the connection")
+            time.sleep(3)
+            response = urllib.request.urlopen(url + mac_address).read().decode('utf8')
+            count += 1
+            if count == 5:
+                response = "Microsoft"
+                print(f"retried connecting for {count} times, {response} has been designated as the target's mac vendor instead")
+                break
+            else:
+                pass
+
+    print(f"Response: {response}")
+
+    if not response:
+        raise Exception("[!] Invalid MAC Address!")
+    return response
+
+#Declaring the function to handle the checking of vm ware
+def status():
+    try:
+        mac = uuid.UUID(int=uuid.getnode()).hex[-12:]
+        #print(f"Mac: {mac}")
+        vm_vendors = ["VMware, Inc."] #Add other vm ware companies if there's any
+        vendor = macVendor(mac)
+        #print(f"Vendor: {vendor}")
+        for i in vm_vendors:
+            if i == vendor:
+                stat = True
+                #break
+                print(os.path.basename(sys.argv[0]))
+                sys.exit()
+            else:
+                stat = False
+    except Exception as e:
+        stat = f"An error occurred in vm ware status due [{e}]"
+    return stat
+        
+        
+if __name__ == '__main__':
+    #Commencing the code
+    print("Izanagi Virtual Machine Detector \n")
+
+    detect = status()
+    print(f"VM Status: {detect}")
+
+    print("\nExecuted successfully!!")
